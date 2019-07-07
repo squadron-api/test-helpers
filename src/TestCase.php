de00df8a6374262ca46f4ba8324611f704240508
Andrey Novikov <c0013r.hofc@gmail.com>
2019-07-07 20:26:00 +0300

    WIP

---
+++ src/TestCase.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace Squadron\Tests;
+
+use Dotenv\Dotenv;
+use Illuminate\Foundation\Application;
+use Orchestra\Testbench\TestCase as Orchestra;
+use ReflectionClass;
+use ReflectionException;
+
+abstract class TestCase extends Orchestra
+{
+    public function setUp(): void
+    {
+        $this->loadEnvironmentVariables();
+
+        parent::setUp();
+    }
+
+    private function loadEnvironmentVariables(): void
+    {
+        if (! file_exists(__DIR__.'/../.env'))
+        {
+            return;
+        }
+
+        Dotenv::create(__DIR__.'/..')->load();
+    }
+
+    /**
+     * Define environment setup.
+     *
+     * @param Application $app
+     */
+    protected function getEnvironmentSetUp($app): void
+    {
+        $dbType = env('DB_TYPE', 'memory');
+
+        if ($dbType === 'memory')
+        {
+            $config = [
+                'driver' => 'sqlite',
+                'database' => ':memory:',
+                'prefix' => '',
+            ];
+        }
+        else
+        {
+            $config = [
+                'driver' => 'mysql',
+                'host' => env('DB_HOST'),
+                'port' => env('DB_PORT'),
+                'database' => env('DB_DATABASE'),
+                'username' => env('DB_USERNAME'),
+                'password' => env('DB_PASSWORD'),
+                'charset' => 'utf8mb4',
+                'collation' => 'utf8mb4_unicode_ci',
+                'prefix' => '',
+                'strict' => true,
+                'engine' => null,
+            ];
+        }
+
+        $app['config']->set('database.default', 'testbench');
+        $app['config']->set('database.connections.testbench', $config);
+    }
+
+    /**
+     * @param Application $app
+     *
+     * @return array
+     */
+    protected function getPackageProviders($app): array
+    {
+        return $this->getServiceProviders();
+    }
+
+    /**
+     * Calls private / protected method.
+     *
+     * @param $obj
+     * @param $name
+     * @param array $args
+     *
+     * @throws ReflectionException
+     *
+     * @return mixed
+     */
+    protected function callMethod($obj, $name, array $args = [])
+    {
+        $class = new ReflectionClass($obj);
+        $method = $class->getMethod($name);
+        $method->setAccessible(true);
+
+        return $method->invokeArgs($obj, $args);
+    }
+
+    abstract protected function getServiceProviders(): array;
+}
